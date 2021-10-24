be1ac52beee6176386b15725312d0262e82d9af0
2KCurry <52842919+2KCurry@users.noreply.github.com>
2021-10-24 10:33:30 -0500

    Update Module.lua

--- Module.lua
+++ Module.lua
@@ -36,11 +36,11 @@ local tableinsert = table.insert
 -- // Silent Aim Vars
 getgenv().Aiming = {
     Enabled = true,
-    ShowFOV = true,
+    ShowFOV = false,
     FOVSides = 12,
     FOVColour = Color3fromRGB(231, 84, 128),
     VisibleCheck = true,
-    FOV = 20,
+    FOV = 60,
     HitChance = 100,
     Selected = LocalPlayer,
     SelectedPart = nil,
